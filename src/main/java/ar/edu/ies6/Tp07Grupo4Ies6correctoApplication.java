cd53769be5f04d6de22a6303487c80cf3e9d2e93
emina <emina@EDITH>
2024-10-15 14:42:57 -0300

    inicializando repositorio remoto del proyecto del grupo4- Edith Martinez

---
+++ src/main/java/ar/edu/ies6/Tp07Grupo4Ies6correctoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp07Grupo4Ies6correctoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp07Grupo4Ies6correctoApplication.class, args);
+	}
+
+}
