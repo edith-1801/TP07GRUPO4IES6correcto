f0be875c443df0ade0cefda2b278d298e79b3e51
emina <emina@EDITH>
2024-10-21 11:42:45 -0300

    creacion de la clase AlmacenAlumnos y la interface-Edith Martinez

--- src/main/java/ar/edu/ies6/controller/IndexController.java
+++ src/main/java/ar/edu/ies6/controller/IndexController.java
@@ -2,9 +2,11 @@ package ar.edu.ies6.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.ies6.model.Alumno;
+import ar.edu.ies6.service.imp.AlumnoServiceImp;
 
 @Controller
 public class IndexController {
@@ -23,15 +25,27 @@ public class IndexController {
 	public ModelAndView getIndexWithAlumno() {
 		//codigo
 		Alumno unAlumno =new Alumno();
-		unAlumno.setApellido("Martinez");
-		unAlumno.setNombre("Edith");
-		unAlumno.setDni("37305495");
+		
+		//unAlumno.setApellido("Martinez");
+		//unAlumno.setNombre("Edith");
+		//unAlumno.setDni("37305495");
 		
 		
 		ModelAndView transportador = new ModelAndView("index");
 		transportador.addObject("alumno", unAlumno);
 		
 		return transportador;
+	}
+	@PostMapping ("/guardarAlumno")	
+	public ModelAndView guardarAlumno(Alumno alumno) {	
+	
+	
+		AlumnoServiceImp alumnoService = new AlumnoServiceImp();
+		alumnoService.guardarAlumno(alumno);
+		
+	ModelAndView transportador = new ModelAndView("avisoExito");
+	//transportador.addObject("alumno", unAlumno);
+	return transportador;
 		
 		
 		
