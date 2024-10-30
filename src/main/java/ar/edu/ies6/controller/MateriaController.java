37ba944369e209e08e2ef3576de29e6442800abc
Estudiante <Estudiante@192.168.1.10>
2024-10-29 22:10:52 -0300

    agregando evelyn martinez

--- src/main/java/ar/edu/ies6/controller/MateriaController.java
+++ src/main/java/ar/edu/ies6/controller/MateriaController.java
@@ -1,16 +1,51 @@
 package ar.edu.ies6.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.ies6.model.Materia;
+import ar.edu.ies6.service.IMateriaService;
+import ar.edu.ies6.service.imp.MateriaServiceImp;
+import ar.edu.ies6.util.AlmacenMaterias;
 
 @Controller
 public class MateriaController {
-	@GetMapping ("/materias")
-	public String getMaterias() {
-		//codigo
-		return "formMateria";
-	}
+	@Autowired
+	Materia unMateria;
 	
+	@Autowired
+	
+	IMateriaService materiaService;
+	@GetMapping ("/materias")
+	public  ModelAndView getWithMaterias() {
+		//codigo
+		//creado la materia
+		Materia unMateria = new Materia();
+		//unMateria.setNombre("Matematica");
+		//patron de diseño SINGLETON
+		//unMateria.setCarrera("informatica");
+		//unMateria.setAño("2024");
+		//unMateria.setCodigo("11");
+		
+		//transporte hacia la vista
+		ModelAndView transportador = new ModelAndView("formMateria");
+		transportador.addObject ("materia", unMateria);
+		
+		return transportador;
+	}
+	@PostMapping ("/guardarMateria")
+public  ModelAndView guardarMateria(Materia materia) {
 
+//MateriaServiceImp materiaService = new MateriaServiceImp ();
+materiaService.guardarMateria(materia);
+
+ModelAndView transportador = new ModelAndView("listaMateria");
+transportador.addObject ("listadoMateria", materiaService.listarTodosMateria());
+ return transportador;
 }
 
+}
+	
---
+++ src/main/java/ar/edu/ies6/service/IMateriaService.java
@@ -0,0 +1,20 @@
+package ar.edu.ies6.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ar.edu.ies6.model.Materia;
+@Service
+public interface IMateriaService {
+	//crud
+		//metodos
+		public void guardarMateria(Materia materia);
+		public void eliminarMateria(String codigo);
+		public void modificarMateria(Materia materiaModificado);
+		public Materia consultarMateria(String codigo);
+		public List<Materia> listarTodosMateria();
+		
+
+
+}
